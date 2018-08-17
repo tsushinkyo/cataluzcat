06e9a54070fec432593aabc87c13c9b4356652e5
Alejandro Chacon <tsushinkyo@gmail.com>
2018-08-17 13:34:52 +0200

    updated app

--- server/app.js
+++ server/app.js
@@ -8,17 +8,26 @@ var app = express();
 // Set up database conection
 var mongoDB = 'mongodb://127.0.0.1:27017/cataluz';
 mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
 var db = mongoose.connection;
+// Models
+var Schema = mongoose.Schema;
+var categorySchema = new Schema({
+    code: Number,
+    name: String,
+});
+var Category = mongoose.model('Category', categorySchema);
 // Middleware
 app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
 app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
-//Ejemplo: GET http://localhost:8080/items
+//Ejemplo: GET http://localhost:8080/categories
 app.get('/categories', function (req, res, next) {
-    res.send('Get categories');
+    // res.send('Get categories');
+    Category.find({}, function (err, categories) {
+        res.send(categories);
+    });
 });
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 exports.default = app;
--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ const app = express();
 
 // Set up database conection
 let mongoDB = 'mongodb://127.0.0.1:27017/cataluz'
+
 mongoose.connect(mongoDB);
 let db = mongoose.connection;
 
@@ -28,16 +29,15 @@ app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
 app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
 
 
-//Ejemplo: GET http://localhost:8080/items
+//Ejemplo: GET http://localhost:8080/categories
 app.get('/categories', function(req, res, next) {
-    res.send('Get categories');
     Category.find({}, function (err, categories) {
         res.send(categories); 
     });
 });
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
