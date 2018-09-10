d962608d91263693dfbc3135011ea21eecf4f0fd
Alejandro Chacon <tsushinkyo@gmail.com>
2018-09-10 16:41:58 +0200

    started admin schafold

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,6 +3,24 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +28,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [],
   schemas: [
