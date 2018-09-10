0fa30f4278b184e1eb024031d98ded0ab9e30e6f
Alejandro Chacon <tsushinkyo@gmail.com>
2018-09-10 16:24:58 +0200

    add material

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   schemas: [
